606c0561ffdba4792192aed2d16948606a9d7f74
Jos3ki <joosec29@gmail.com>
2026-03-08 15:23:48 -0700

    crear la clase Usuario

---
+++ parcial-1-poo/practica-4/clases/Usuario.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Clase base Usuario.
+ * Aplica encapsulamiento y validación de datos en el constructor.
+ *
+ * @package Usuarios
+ * @author Jose Carlos
+ */
+class Usuario {
+
+    /** @var string */
+    protected $vNombre;
+    
+    /** @var string */
+    protected $vCorreo;
+
+    /**
+     * Constructor con validación de correo.
+     *
+     * @param string $nombre
+     * @param string $correo
+     * @throws Exception Si el correo no tiene un formato válido.
+     */
+    public function __construct($nombre, $correo)
+    {
+        $this->vNombre = $nombre;
+
+        if(filter_var($correo, FILTER_VALIDATE_EMAIL)){
+            throw new Exception( "correo invalido: " . $correo);
+        }
+
+        $this->vCorreo = $correo;
+    }
+
+    /** @return string */
+    public function getNombre() {
+        return $this->vNombre;
+    }
+
+    /** @return string */
+    public function getCorreo() {
+        return $this->vCorreo;
+    }
+
+     /**
+     * @param string $nuevoNombre
+     */
+    public function setNombre($nuevoNombre) {
+        $this->vNombre = $nuevoNombre;
+    }
+
+     /**
+     * @param string $nuevoCorreo
+     */
+    public function setCorreo($nuevoCorreo) {
+        $this->vCorreo = $nuevoCorreo;
+    }
+}
