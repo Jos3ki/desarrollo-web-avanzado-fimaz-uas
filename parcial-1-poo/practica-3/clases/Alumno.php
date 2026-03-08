490312166036e09e517daf1c610a1f14955b33a0
Jos3ki <joosec29@gmail.com>
2026-03-08 14:27:43 -0700

    creacion de la clase Alumno con herencia

---
+++ parcial-1-poo/practica-3/clases/Alumno.php
@@ -0,0 +1,49 @@
+<?php
+
+require 'Usuario.php';
+
+/**
+ * Clase Alumno.
+ * Hereda de Usuario y añade propiedades específicas de un estudiante.
+ *
+ * @package Usuarios
+ * @author Jose Carlos
+ */
+class Alumno extends Usuario {
+
+    /**
+     * @var string
+     */
+    private $vMatricula;
+
+    /**
+     * Constructor del Alumno.
+     *
+     * @param string $nombre
+     * @param string $correo
+     * @param string $matricula
+     */
+    public function __construct($nombre, $correo, $matricula)
+    {
+        parent::__construct($nombre, $correo);
+        $this->vMatricula = $matricula;
+    }
+
+    /**
+     * Obtiene el rol del usuario.
+     *
+     * @return string
+     */
+    public function getRol(){
+        return "Alumno";
+    }
+
+    /**
+     * Obtiene la matrícula del alumno.
+     *
+     * @return string
+     */
+    public function getMatricula(){
+        return $this->vMatricula;
+    }
+}
