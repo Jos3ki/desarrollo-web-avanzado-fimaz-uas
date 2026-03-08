c2c6b044417d954e49e01571cc89628c0104d022
Jos3ki <joosec29@gmail.com>
2026-03-08 14:21:14 -0700

    creacion de la clase Admin que hereda de la clase usuario

---
+++ parcial-1-poo/practica-3/clases/Admin.php
@@ -0,0 +1,22 @@
+<?php 
+
+require 'Usuario.php';
+
+/**
+ * Clase que representa a un Administrador del sistema.
+ * Hereda las propiedades y métodos básicos de Usuario.
+ * @package Usuarios
+ * @version 1.0.0
+ * @author Jose Carlos
+ */
+class Admin extends Usuario {
+
+    /**
+     * Obtiene el rol o nivel de acceso del usuario.
+     *
+     * @return string Retorna el rol asignado.
+     */
+    public function getRol(){
+        return "Administrador";
+    }
+}
